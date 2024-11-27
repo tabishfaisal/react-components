3238b9d595c6c405b40a336421daef79faef35b8
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-11-27 16:09:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <header className='title'>
+          <h1>Il mio blog</h1>
+        </header>
+      </section>
+      <section className='container'>
+        <div className='card'>
+          <img src='./imgs-600x400'></img>
+          <h2>titlo del post</h2>
+
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default App
